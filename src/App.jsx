32e29f02db5ceac89d1688bfa40b227454308f37
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-07-08 21:11:26 +0530

    complete the project, with minimal features but Rdux-Toolkit is completly understood.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
 
 
 function App() {
 
   return (
     <>
-      <h1>React Redux Toolkit Tutorial</h1>
+      <h1 className=" bg-red-500">React Redux Toolkit Tutorial (Todo) </h1>
+      <AddTodo/>
+      <Todos/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux"; // provider is what helps components get the values/data from store i.e the one we created;
+//"Provider takes a prop called store. You pass your actual Redux store object to it. That store is what holds all slices and reducers you defined in your app."
+// store is like a key that ask for referance of the store(that have all the slices);
+// so provider connects React components with the help of it's key "state" where we pass the store we created.
+import { store } from "./app/store.jsx"; // store the one we create that hold all the slices/reducers;
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}> 
+      <App />
+    </Provider>
+  </StrictMode>
+);
